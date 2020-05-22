9f85db4ca9ec029269e37216972aed103396ead3
DHEERAJ MAURYA <dheeraj945016@gmail.com>
2020-05-22 15:58:53 +0530

    Updates

---
+++ precache-manifest.a23adf16b1e8ec4a47bbb665b3c68f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4df81d08750ef91ceb0b54672333fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87f2dbac3560c505358a",
+    "url": "/static/css/main.b9dd1557.chunk.css"
+  },
+  {
+    "revision": "2cdb9241612e80a25013",
+    "url": "/static/js/2.229943ad.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.229943ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f2dbac3560c505358a",
+    "url": "/static/js/main.cc1fb9e1.chunk.js"
+  },
+  {
+    "revision": "1e2fef1b0311b6addbef",
+    "url": "/static/js/runtime-main.98da117b.js"
+  }
+]);
